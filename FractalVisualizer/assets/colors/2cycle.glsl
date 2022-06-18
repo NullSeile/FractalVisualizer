02ded4cf4c9f7b4503b8f14205ec3d725bc2b5c1
nullptr <eliesbertran@gmail.com>
2022-06-18 23:34:27 +0200

    Updated default values in the 2cycle color function

--- FractalVisualizer/assets/colors/2cycle.glsl
+++ FractalVisualizer/assets/colors/2cycle.glsl
@@ -1,7 +1,7 @@
 #uniform float colorMult 100 1 1 NULL false;
 #uniform float offset 0 0.01 -1 1;
-#uniform color c1 0.00 0.00 0.00;
-#uniform color c2 1.00 1.00 1.00;
+#uniform color c1 0.13 0.08 0.20;
+#uniform color c2 0.92 0.83 0.49;
 #uniform bool sine_interpolation false;
 
 #define PI 3.1415926535

96bec913820c9cdd2411f12e7705af8c26341cf5
nullptr <eliesbertran@gmail.com>
2022-11-14 11:32:00 +0100

    Changed defaut 3cycle colors

--- FractalVisualizer/assets/colors/3cycle.glsl
+++ FractalVisualizer/assets/colors/3cycle.glsl
@@ -1,8 +1,8 @@
 #uniform float colorMult 100 1 1 NULL false;
 #uniform float offset 0 0.01 -1 1;
-#uniform color c1 0.20 0.10 0.07;
-#uniform color c2 0.14 0.39 0.45;
-#uniform color c3 0.96 0.70 0.18;
+#uniform color c1 0.07 0.14 0.25;
+#uniform color c2 0.72 0.91 0.83;
+#uniform color c3 0.96 0.6 0.35;
 #uniform bool sine_interp true;
 
 #define PI 3.1415926535
